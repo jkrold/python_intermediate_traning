2fc35ab2fdf63b6e6603ac5b10ca0c0bab4476c7
me@example.pl <krol.jadwiga@vp.pl>
2021-01-31 13:13:59 +0100

    Added in Cat class eat_mouse method, which add 1 mouse to number_of_eaten_mice list

--- sda_exercises_oop_1/cat.py
+++ sda_exercises_oop_1/cat.py
@@ -1,6 +1,11 @@
 class Cat:
     def __init__(self, name: str):
         self._name = name
+        self._number_of_eaten_mice = 0
 
     def make_sound(self) -> str:
-        print(f"{self._name}: Meow, meow")
+        return f"{self._name}: Meow, meow"
+
+    def eat_mouse(self) -> str:
+        self._number_of_eaten_mouse += 1
+        return f'{self._name}: I ate {self._number_of_eaten_mouse} mice'
--- sda_exercises_oop_1/main.py
+++ sda_exercises_oop_1/main.py
@@ -1,11 +1,25 @@
 from sda_exercises_oop_1.cat import Cat
 
-
-def main():
-    pass
-
 if __name__ == '__main__':
 
     cats_list = [Cat('Snow flake'), Cat('Salem'), Cat('Tiger'), Cat('Smokey')]
     for cat in cats_list:
-        cat.make_sound()
+        sound: str = cat.make_sound()
+        print(sound)
+
+    for i in range(0, 4):
+        cat1_num_of_eaten_mice = cats_list[0].eat_mouse()
+
+    for i in range(0, 2):
+        cat2_num_of_eaten_mice = cats_list[1].eat_mouse()
+
+    for i in range(0, 5):
+        cat3_num_of_eaten_mice = cats_list[2].eat_mouse()
+
+    for i in range(0, 1):
+        cat4_num_of_eaten_mice = cats_list[3].eat_mouse()
+
+    print(cat1_num_of_eaten_mice)
+    print(cat2_num_of_eaten_mice)
+    print(cat3_num_of_eaten_mice)
+    print(cat4_num_of_eaten_mice)
