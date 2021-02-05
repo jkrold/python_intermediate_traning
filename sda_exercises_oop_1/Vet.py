7b3995bcbb24928e6a264bab7c0a6ce3d006483d
me@example.pl <krol.jadwiga@vp.pl>
2021-02-05 22:26:03 +0100

    Implemented Vet class and sayDogHello method

--- sda_exercises_oop_1/Vet.py
+++ sda_exercises_oop_1/Vet.py
@@ -1,7 +1,10 @@
 from python_intermediate_traning.sda_exercises_oop_1.cat import Cat
+from python_intermediate_traning.sda_exercises_oop_1.dog import Dog
 
 
 class Vet:
     def sayCatHello(self, feline_patient: Cat):
         print(f"Hello {feline_patient.name}")
 
+    def sayDogHello(self, canine_patient: Dog):
+        print(f"Hello {canine_patient.name}")
