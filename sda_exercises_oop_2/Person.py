c028135e089dd33cdd6c9ea6256d1f36d6632d1a
me@example.pl <krol.jadwiga@vp.pl>
2021-02-06 22:11:09 +0100

    Implemented propets and setters in Person class

--- sda_exercises_oop_2/Person.py
+++ sda_exercises_oop_2/Person.py
@@ -1,7 +1,34 @@
 from datetime import date
 
+
 class Person:
-    def __init__(self, first_name: str, last_name: str, birthsday: date):
+    def __init__(self, first_name: str, last_name: str, day_of_birth: date):
         self._first_name = first_name
         self._last_name = last_name
-        self._birthsday = birthsday
+        self._day_of_birth = day_of_birth
+
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @property
+    def day_of_birth(self):
+        return self._day_of_birth
+
+    @first_name.setter
+    def first_name(self, first_name: str):
+        self._first_name = first_name
+
+    @last_name.setter
+    def last_name(self, last_name: str):
+        self._last_name = last_name
+
+    @first_name.setter
+    def day_of_birth(self, day_of_birth: date):
+        self._day_of_birth = day_of_birth
+
+
