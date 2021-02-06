3f18dffc51cc4d386e62c2280ce3ee9c110a9d3d
me@example.pl <krol.jadwiga@vp.pl>
2021-02-06 22:47:47 +0100

    Implemented Employee class in Person.py

--- sda_exercises_oop_2/Person.py
+++ sda_exercises_oop_2/Person.py
@@ -27,8 +27,41 @@ class Person:
     def last_name(self, last_name: str):
         self._last_name = last_name
 
-    @first_name.setter
+    @day_of_birth.setter
     def day_of_birth(self, day_of_birth: date):
         self._day_of_birth = day_of_birth
 
+class Employee(Person):
+    def __init__(self, first_name: str, last_name: str, day_of_birth: date):
+        day_of_birth = self.check_birthsday(day_of_birth)
+        super().__init__(first_name, last_name, day_of_birth)
 
+    @staticmethod
+    def check_birthsday(day_of_birth: date):
+        if not 1900 <= day_of_birth.year <= 2020:
+            day_of_birth = 0
+        return day_of_birth
+
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @property
+    def day_of_birth(self):
+        return self._day_of_birth
+
+    @first_name.setter
+    def first_name(self, first_name: str):
+        self._first_name = first_name
+
+    @last_name.setter
+    def last_name(self, last_name: str):
+        self._last_name = last_name
+
+    @day_of_birth.setter
+    def day_of_birth(self, day_of_birth: date):
+        self._day_of_birth = self.check_birthsday(day_of_birth)
