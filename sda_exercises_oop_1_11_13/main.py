f84c4106b8d623d716d553f15bdb5421d23b46a9
me@example.pl <krol.jadwiga@vp.pl>
2021-02-06 12:53:09 +0100

    Testing code in main.py file

--- sda_exercises_oop_1_11_13/main.py
+++ sda_exercises_oop_1_11_13/main.py
@@ -1,10 +1,14 @@
-from python_intermediate_traning.sda_exercises_oop_1_11_13.Figure import Rectangle, Circle, Triangle
+from python_intermediate_traning.sda_exercises_oop_1_11_13.Figure import Rectangle, Circle, Triangle, sum_area_of_figures
+# from python_intermediate_traning.sda_exercises_oop_1_11_13.sum_area_of_figures import sum_area_of_figures
 
 if __name__ == "__main__":
 
     a = 10.2
     b = 3.1
 
-    print(Rectangle(a, b).get_area())
-    print(Circle(5).get_area())
-    print(Triangle(a, b).get_area())
+    Figure1 = Rectangle(a, b)
+    Figure2 = Circle(5)
+    Figure3 = Triangle(a, b)
+
+    figures = (Figure1, Figure2, Figure3)
+    print(sum_area_of_figures(Figure1, Figure2, Figure3))
