60eb3cc3fa5c3ea7a812e8ee285e7285e1ff3cab
me@example.pl <krol.jadwiga@vp.pl>
2021-02-06 12:52:23 +0100

    Added metgon sum area of figures, which get list of Figures

--- sda_exercises_oop_1_11_13/sum_area_of_figures.py
+++ sda_exercises_oop_1_11_13/sum_area_of_figures.py
@@ -1,5 +1,5 @@
 def sum_area_of_figures(figure_list: list) -> float:
-    summed_area = 0
+    summed_area: float = 0
     for figure in figure_list:
         summed_area += figure.get_area()
-    return summed_area
+    return round(summed_area, 2)
