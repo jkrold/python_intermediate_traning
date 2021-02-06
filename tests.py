cadc2f1500d24976ba70bfd44213f76285e296b8
me@example.pl <krol.jadwiga@vp.pl>
2021-02-06 21:16:02 +0100

    Added tests of enough paint method

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ from python_intermediate_traning.sda_exercises_oop_1_11_13.Figure import Circle,
     sum_area_of_figures
 
 
-def test_check_area():
+def test_check_sum_of_area():
     # given
     circle1 = Circle(1)
     triangle1 = Triangle(2, 1)
@@ -14,6 +14,27 @@ def test_check_area():
     result = sum_area_of_figures([circle1, triangle1, rectangle1])
     print(result)
 
-    assert result == 8.2
+    assert result == 8.14
 
-print(test_check_area())
+def test_enough_paint_method():
+    # given
+    circle1 = Circle(1)
+    triangle1 = Triangle(2, 1)
+    rectangle1 = Rectangle(2, 2)
+
+    can_of_paint = 9
+
+    assert enough_paint(can_of_paint, [circle1, triangle1, rectangle1]) == True
+
+def test_enough_paint_method_2():
+    # given
+    circle1 = Circle(1)
+    triangle1 = Triangle(2, 1)
+    rectangle1 = Rectangle(2, 2)
+
+    can_of_paint = 3
+
+    assert enough_paint(can_of_paint, [circle1, triangle1, rectangle1]) == False
+
+
+print(test_check_sum_of_area())
