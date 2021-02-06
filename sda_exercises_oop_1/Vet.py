1245fa9bcb19e60dd35dc994ddb42b8fd690c168
me@example.pl <krol.jadwiga@vp.pl>
2021-02-06 10:59:45 +0100

    Modified sayHello method - refers to Animal class

--- sda_exercises_oop_1/Vet.py
+++ sda_exercises_oop_1/Vet.py
@@ -1,10 +1,8 @@
+from python_intermediate_traning.sda_exercises_oop_1.Animal import Animal
 from python_intermediate_traning.sda_exercises_oop_1.cat import Cat
 from python_intermediate_traning.sda_exercises_oop_1.dog import Dog
 
 
 class Vet:
-    def sayCatHello(self, feline_patient: Cat):
-        print(f"Hello {feline_patient.name}")
-
-    def sayDogHello(self, canine_patient: Dog):
-        print(f"Hello {canine_patient.name}")
+    def say_hello(self, patient: Animal):
+        print(f"Hello {patient.name}")
