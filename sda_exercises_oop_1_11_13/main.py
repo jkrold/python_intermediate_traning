095614cce3db6fadf271f62c21807e4ab938aa1f
me@example.pl <krol.jadwiga@vp.pl>
2021-02-06 11:39:15 +0100

    Testing code in main.py file

--- sda_exercises_oop_1_11_13/main.py
+++ sda_exercises_oop_1_11_13/main.py
@@ -1,2 +1,10 @@
+from python_intermediate_traning.sda_exercises_oop_1_11_13.Figure import Rectangle, Circle, Triangle
+
 if __name__ == "__main__":
-    pass
+
+    a = 10.2
+    b = 3.1
+
+    print(Rectangle(a, b).get_area())
+    print(Circle(5).get_area())
+    print(Triangle(a, b).get_area())
