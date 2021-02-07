2d96df5fd608980c84cdd67e164f5d8817928876
me@example.pl <krol.jadwiga@vp.pl>
2021-02-07 13:37:51 +0100

    Implemented pickle_read method

--- serialize/pickle_training.py
+++ serialize/pickle_training.py
@@ -8,3 +8,15 @@ def pickle_write(items: list):
             print(pickle.dumps(items))
     except (IOError, Exception) as e:
         print(f'Exception while writing pickle format: {e.args}')
+
+
+def pickle_read():
+    string_list = []
+    try:
+        with open('./data.pickle', 'rb') as fd:
+            string_list = pickle.load(fd)
+    except (IOError, Exception) as e:
+        print(f'Exception while writing pickle format: {e.args}')
+    return string_list
+
+
