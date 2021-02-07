162182802f2d86242c6eecddc5dc2f775ad7be4c
me@example.pl <krol.jadwiga@vp.pl>
2021-02-07 11:43:30 +0100

    Added cas_6

--- Exeption/exercise.py
+++ Exeption/exercise.py
@@ -1,20 +1,46 @@
+import sys
+
+
 def case_1():
     list_of_numbers = [1, 5, 8, 10, 21]
     print("Case_1 before")
     try:
         result = list_of_numbers[5]
     except IndexError as ie:
-        print(f'Exception cought {ie.args}')
+        print(f'Exception caught {ie.args}')
     except Exception as exp:
-        print(f'Exception cought {exp.args}')
+        print(f'Exception caught {exp.args}')
     print("Case_1 after")
 
     try:
         result = list_of_numbers[5]
     except (IndexError, Exception) as e:
-        print(f'Exception cought {e.args}')
+        print(f'Exception caught {e.args}')
 
 def case_2(name: str):
     if len(name) <= 0:
         raise ValueError('Sting is empty')
     print(f'Given name is: {name}')
+
+def case_3(number: int, divisor: int) -> float:
+    result = 0
+    try:
+        result = number / divisor
+    except ZeroDivisionError as zde:
+        print(f'Nie dziel przez zero')
+        result = sys.float_info.max
+        # result = float(sys.maxsize)
+    return result
+
+
+def case_4(dictionary: dict):
+    key = "items"
+    items: list = dictionary[key]
+    # items: list = dictionary.get(key)
+    for item in items:
+        print(item)
+
+
+def case_6():
+    raise NotImplementedError("Solved future")
+
--- Exeption/main.py
+++ Exeption/main.py
@@ -1,10 +1,27 @@
-from python_intermediate_traning.Exeption.exercise import case_1, case_2
+from python_intermediate_traning.Exeption.exercise import case_1, case_2, case_4, case_6
 
 
 def main():
-    print("Start app:")
-    case_2("")
-    print("Finish")
+    # print("Start app:")
+    # try:
+    #     case_2("")
+    # except Exception as e:
+    #     print(f'Exception caught {e.args}')
+    # print("Finish")
+
+    # dictionary = {
+    #     "product": ['butter', 'bread', 'tomato']
+    # }
+    #
+    # try:
+    #     case_4(dictionary)
+    # except KeyError as k:
+    #     print(f'Exception caught {k.args}')
+
+    try:
+        case_6()
+    except NotImplementedError as n:
+        print(f'Exception caught {n.args}')
 
 
 if __name__ == "__main__":
