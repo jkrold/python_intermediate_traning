d9a61393fdf6f97b7107cadf73b6a1e328cf3e2e
me@example.pl <krol.jadwiga@vp.pl>
2021-02-07 13:38:57 +0100

    Checked implemented method

--- serialize/main.py
+++ serialize/main.py
@@ -1,10 +1,19 @@
-from python_intermediate_traning.serialize.pickle_training import pickle_write
+from python_intermediate_traning.serialize.csv_training import csv_writer, csv_read
+from python_intermediate_traning.serialize.pickle_training import pickle_write, pickle_read
 
 
 def main():
-    abc = ['bread', 'butter', 'cola']
-    pickle_write(abc)
+    # abc = ['bread', 'butter', 'cola']
+    # pickle_write(abc)
+    # print(pickle_read())
 
+    users = [
+        ('John', "Smith", 765438),
+        ('Samuel', "Jackson", 668438)
+    ]
+
+    # csv_writer(users)
+    print(csv_read())
 
 if __name__ == "__main__":
     main()
