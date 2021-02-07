c87f31b5e8966c3fdb3850c5c4bacda49e26c0b7
me@example.pl <krol.jadwiga@vp.pl>
2021-02-07 09:52:25 +0100

    implementes who_i_am method

--- sda_exercises_oop_2/Person.py
+++ sda_exercises_oop_2/Person.py
@@ -32,9 +32,10 @@ class Person:
         self._day_of_birth = day_of_birth
 
 class Employee(Person):
-    def __init__(self, first_name: str, last_name: str, day_of_birth: date):
+    def __init__(self, first_name: str, last_name: str, day_of_birth: date, salary: int = 1000):
         day_of_birth = self.check_birthsday(day_of_birth)
         super().__init__(first_name, last_name, day_of_birth)
+        self._salary = salary
 
     @staticmethod
     def check_birthsday(day_of_birth: date):
@@ -54,6 +55,10 @@ class Employee(Person):
     def day_of_birth(self):
         return self._day_of_birth
 
+    @property
+    def salary(self):
+        return self._salary
+
     @first_name.setter
     def first_name(self, first_name: str):
         self._first_name = first_name
@@ -65,3 +70,6 @@ class Employee(Person):
     @day_of_birth.setter
     def day_of_birth(self, day_of_birth: date):
         self._day_of_birth = self.check_birthsday(day_of_birth)
+
+    def who_i_am(self):
+        return f"I'm {self._first_name} {self._last_name} and I earn {self._salary}zl"
