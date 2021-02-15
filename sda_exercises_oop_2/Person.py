fea31ad53bd6cae106996780a4931d4e31a6ae8b
me@example.pl <krol.jadwiga@vp.pl>
2021-02-15 20:26:38 +0100

    Added Manager class, who earn 10% of salary more than Employee

--- sda_exercises_oop_2/Person.py
+++ sda_exercises_oop_2/Person.py
@@ -31,6 +31,7 @@ class Person:
     def day_of_birth(self, day_of_birth: date):
         self._day_of_birth = day_of_birth
 
+
 class Employee(Person):
     def __init__(self, first_name: str, last_name: str, day_of_birth: date, salary: int = 1000):
         day_of_birth = self.check_birthsday(day_of_birth)
@@ -44,32 +45,27 @@ class Employee(Person):
         return day_of_birth
 
     @property
-    def first_name(self):
-        return self._first_name
-
-    @property
-    def last_name(self):
-        return self._last_name
-
-    @property
-    def day_of_birth(self):
-        return self._day_of_birth
-
-    @property
     def salary(self):
         return self._salary
 
-    @first_name.setter
-    def first_name(self, first_name: str):
-        self._first_name = first_name
-
-    @last_name.setter
-    def last_name(self, last_name: str):
-        self._last_name = last_name
-
-    @day_of_birth.setter
-    def day_of_birth(self, day_of_birth: date):
-        self._day_of_birth = self.check_birthsday(day_of_birth)
+    @salary.setter
+    def salary(self, salary: int):
+        self._salary = salary
 
     def who_i_am(self):
         return f"I'm {self._first_name} {self._last_name} and I earn {self._salary}zl"
+
+
+class Manager(Employee):
+    def __init__(self, first_name: str, last_name: str, day_of_birth: date, salary: int = 1000):
+        day_of_birth = self.check_birthsday(day_of_birth)
+        super().__init__(first_name, last_name, day_of_birth, salary)
+
+    @property
+    def salary(self):
+        return int(self._salary * 1.1)
+
+    def who_i_am(self):
+        return f"I'm Manager {self._first_name} {self._last_name} and I earn {self._salary}zl"
+
+
