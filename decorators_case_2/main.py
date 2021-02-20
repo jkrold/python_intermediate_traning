972e3b2d8ce2ebde8f1e0780d1a789c090550595
me@example.pl <krol.jadwiga@vp.pl>
2021-02-20 14:09:42 +0100

    Decorators ex

--- decorators_case_2/main.py
+++ decorators_case_2/main.py
@@ -1,4 +1,17 @@
+from functools import wraps
+
+
 def catch_io_error_decorator(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except IOError as e:
+            print(f'IOError cought, more info: {e.args}')
+            return None
+    return inner
+
+def catch_io_error_with_wraps(func):
+    @wraps(func)
     def inner(*args, **kwargs):
         try:
             return func(*args, **kwargs)
@@ -11,9 +24,15 @@ def catch_io_error_decorator(func):
 def throw_exception_file():
     raise IOError("Test error")
 
+@catch_io_error_with_wraps
+def read_does_not_exist_file():
+    with open("./not_exist.txt", "r") as fd:
+        fd.read()
+
 
 def main():
-    throw_exception_file()
+    # throw_exception_file()
+    read_does_not_exist_file()
 
 if __name__ == '__main__':
     main()
