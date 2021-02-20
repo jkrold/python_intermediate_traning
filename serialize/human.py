c5b8cdb9e927136923c8d53cee64e279f8ccdca3
me@example.pl <krol.jadwiga@vp.pl>
2021-02-20 10:20:15 +0100

    Ex 5 json

--- serialize/human.py
+++ serialize/human.py
@@ -13,16 +13,24 @@ class Human:
     def convert_to_dict(self):
         return self.__dict__
 
+    @classmethod
+    def convert_from_dict(cls, params: dict):
+        name = params.get("name", "-")
+        surname = params.get("surname", "-")
+        age = params.get("age", 0)
+
+        return cls(age, name, surname)
+
+
 def json_human_to_file(humans: list):
     humans_serialize = []
 
     for human in humans:
         human_dict = human.convert_to_dict()
-        # human_json = json.dumps(human_dict)
         humans_serialize.append(human_dict)
 
     try:
-        with open("./humans.json", "w") as fd:
+        with open("./humans_test.json", "w") as fd:
             json.dump(humans_serialize, fd, indent=2)
     except (IOError, Exception) as e:
         print(f'Problem with writing to file, more info: {e.args}')
