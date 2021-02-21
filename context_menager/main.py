fbb49987ffbaf6f6156b4a399299e6c86e848d85
me@example.pl <krol.jadwiga@vp.pl>
2021-02-21 11:08:20 +0100

    Add Exercise 2 and 3

--- context_menager/main.py
+++ context_menager/main.py
@@ -1,8 +1,9 @@
 from context_menager.exercise_1 import exercise_1
+from context_menager.exercise_2 import exercise_2
 
 
 def main():
-    exercise_1()
+    exercise_2()
 
 if __name__ == '__main__':
     main()
