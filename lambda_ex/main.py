af0f5db0f0f0f5562cd6a8b958b11e2ef4d86f4d
me@example.pl <krol.jadwiga@vp.pl>
2021-02-24 21:17:28 +0100

    lambda exercises

---
+++ lambda_ex/main.py
@@ -0,0 +1,10 @@
+from lambda_ex.exercise_1 import exercise_1, exercise_2_3, exercise_4
+
+
+def main():
+    exercise_1()
+    exercise_2_3()
+    exercise_4()
+
+if __name__ == '__main__':
+    main()
